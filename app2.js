8f2cfb592ad94c0031cf7643ab5476ee9c784be0
vivekmahey <vivekmahey2244@gmail.com>
2025-08-02 16:53:55 +0530

    Applied conditional statements and loops in EJS to render dynamic product list

--- app2.js
+++ app2.js
@@ -14,6 +14,20 @@ app.get("/about", (req, res) => {
   res.render("about", { pageTitle: "About Us", username: "Vivek" });
 });
 
+// Loops and condiationals 
+app.get('/users',(req,res)=>{
+
+    const users=[
+    { name: "Vivek", role: "admin" },
+    { name: "Aman", role: "user" },
+    { name: "Sara", role: "user" },
+    { name: "Rohan", role: "admin" }
+    ];
+
+    res.render('users', {pageTitle: "Users Page", users})
+});
+
+
 app.listen(port ,()=>{
     console.log(`Server is running on port http://localhost:${port}`);
 });
