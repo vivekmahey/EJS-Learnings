da444f39556ccb3e3aa24600434a6c61ed5202ba
vivekmahey <vivekmahey2244@gmail.com>
2025-08-02 14:33:13 +0530

    Completed EJS modules 1 to 3: setup, syntax, partials, routing

---
+++ app.js
@@ -0,0 +1,14 @@
+const express=require("express");
+const app=express();
+const port=3000;
+
+app.set("view engine","ejs");
+// Setting home route
+
+app.get("/",(req,res)=>{
+    res.render("home",{username:"Vivek"})
+});
+
+app.listen(port,()=>{
+    console.log(`Server Start Listening at http://localhost:${port}`);
+});
