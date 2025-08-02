d246543fd04185bf439f496ee103fafdf2d7993b
vivekmahey <vivekmahey2244@gmail.com>
2025-08-02 15:08:04 +0530

    Implemented EJS Basic Syntax examples with dynamic data

--- app.js
+++ app.js
@@ -3,10 +3,21 @@ const app=express();
 const port=3000;
 
 app.set("view engine","ejs");
-// Setting home route
 
+// Setting home route
 app.get("/",(req,res)=>{
     res.render("home",{username:"Vivek"})
+});
+
+// Understanding Syntaxes
+
+app.get("/syntax", (req, res) => {
+    res.render("syntax", {
+        user: "Vivek",
+        age: 22,
+        skills: ["HTML", "CSS", "JavaScript", "EJS"],
+        isLoggedIn: true
+    });
 });
 
 app.listen(port,()=>{
